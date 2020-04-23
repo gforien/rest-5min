2bd061d1e0fe1826af9e0d328ba55ed440fb20f5
Gabriel Forien <gforien@gmail.com>
2020-04-24 00:19:22 +0200

    test corrected :ambulance::pencil:

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  *        env vars        *
  **************************/
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || "";
 
 
 /***************************************
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,6 @@
 /******************************
  *        dependencies        *
  ******************************/
-const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI;
-
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
@@ -24,8 +21,8 @@ describe('Back-end testing', () => {
     this.log.restore();
   });
 
-  it(`should log "Listening on port ${PORT}"`, () => {
-    let expected = `Listening on port ${PORT}`;
+  it('should log "Listening on port 5000"', () => {
+    let expected = 'Listening on port 5000';
 
     console.log(expected);
     expect(console.log.calledWith(expected)).to.be.true;
@@ -37,7 +34,7 @@ describe('routes testing', () => {
 
   const server = require('../index');
 
-  it('should have status 200 and log a simple sentence', () => {
+  it('should have status 200 and log a simple sentence', (done) => {
 
     chai
       .request(server)
@@ -46,6 +43,7 @@ describe('routes testing', () => {
         expect(err).to.equal(null);
         expect(res.status).to.equal(200);
         expect(res.text).to.equal('Bravo ! You made it to the front page');
+        done();
       });
 
   });
